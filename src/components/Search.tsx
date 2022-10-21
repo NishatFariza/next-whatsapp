800f2575beca059b2f4c78b3cb8f7aa7da20cfe4
NishatFariza <nishatfarzia78@gmail.com>
2022-10-22 01:43:14 +0600

    complete contactbar

---
+++ src/components/Search.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div className="h-16 p-2">
+          <input
+              placeholder="Search or start new chat"
+        className="bg-slate-200 w-full h-full rounded-md focus:outline-none px-3 "
+        type="search"
+        name=""
+        id=""
+      />
+    </div>
+  );
+};
+
+export default Search;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import AuthenticatedUser from "@/components/AuthenticatedUser";
 import ContactCard from "@/components/ContactCard";
+import Search from "@/components/Search";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -10,6 +11,7 @@ const Home: NextPage = () => {
     <div className="chat-app">
       <div className="chat-app__sidebar">
         <AuthenticatedUser />
+        <Search />
         <div className="chat-app__contacts">
           <ContactCard />
           <ContactCard />
