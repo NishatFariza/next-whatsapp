03976128546329329d65d00deefe96abbaff26d3
NishatFariza <nishatfarzia78@gmail.com>
2022-10-22 00:43:14 +0600

    wip:authentication user

---
+++ src/components/AuthenticatedUser.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const AuthenticatedUser = () => {
+  return (
+    <div className="chat-app__contacts flex justify-between border-b p-3 shadow">
+      <button>
+        <img
+          className="w-10 h-10 rounded-full"
+          src="https://i.pinimg.com/236x/b7/1f/95/b71f95d4d07c8949e8e046bab7c7668d.jpg"
+          alt=""
+        />
+      </button>
+      <div>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AuthenticatedUser;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,14 @@
+import AuthenticatedUser from "@/components/AuthenticatedUser";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-
-
-
-
 const Home: NextPage = () => {
   return (
     <div className="chat-app">
       <div className="chat-app__sidebar">
-        <h2>Sidebar</h2>
-        <div className="chat-app__contacts">
-          
-        </div>
+        <AuthenticatedUser />
       </div>
       <div className="chat-app__main">content</div>
     </div>
