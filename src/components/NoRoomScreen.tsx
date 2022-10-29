17f4036213ece386e7fd5dadd373d8fbc649b8cd
NishatFariza <nishatfarzia78@gmail.com>
2022-10-29 07:31:21 +0600

    wip:NoRoomScreen

---
+++ src/components/NoRoomScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoRoomScreen = () => {
+  return (
+    <div className=''></div>
+  )
+}
+
+export default NoRoomScreen;
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Search = () => {
   return (
-    <div className="h-16 p-2">
+    <div className="h-16 p-2 border">
           <input
               placeholder="Search or start new chat"
-        className="bg-slate-200 w-full h-full rounded-md focus:outline-none px-3 "
+        className="bg-slate-200 w-full h-full rounded-md focus:outline-none px-3"
         type="search"
         name=""
         id=""
